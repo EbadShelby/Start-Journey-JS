4ad5292804f65b6307fcce5960b376e4c77b42f6
EbadShelby <ebadyasser123xd@gmail.com>
2025-01-02 08:48:51 +0800

    oop exercise 1.1

--- javascript-amazon-project/data/car.js
+++ javascript-amazon-project/data/car.js
@@ -1,6 +1,16 @@
 class Car {
   brand;
   model;
+
+  constructor(carDetails) {
+    this.brand = carDetails.brand;
+    this.model = carDetails.model;
+  }
 }
 
-const car1 = new Car();
+const car1 = new Car({
+  brand: "Toyota",
+  model: "Corolla",
+});
+
+console.log(car1);
